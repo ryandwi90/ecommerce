b53eef7d9d72775e3f4198f63afdeb95bed95dad
Ryant <ryantdwitomisaelchristian@gmail.com>
2025-04-12 21:19:35 +0700

    produk

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProdukController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/produk", [ProdukController::class, "produk"]);
 
+Route::get("/category", [CategoryController::class, "category"]);
+Route::get("/tambah_category", [CategoryController::class, "tambah_category"]);
+Route::get("/edit_category", [CategoryController::class, "edit_category"]);
